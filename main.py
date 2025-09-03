096edf97952e94ddf7fcc356096e6438c8c25d75
Hadi Moeinnia <hadimm75@gmail.com>
2025-09-03 15:34:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from main_window import MainWindow
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
+    try:
+        with open('stylesheet.qss', 'r') as f:
+            style = f.read()
+            app.setStyleSheet(style)
+    except FileNotFoundError:
+        print("Warning: stylesheet.qss not found. Using default style.")
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
